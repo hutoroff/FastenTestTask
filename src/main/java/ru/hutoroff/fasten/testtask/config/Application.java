f1c599497864a697143c18f34722f326c7b47af1
hutoroff <hutoroff@gmail.com>
2017-02-14 21:12:15 +0300

    Fixed error on application start caused by Spring Boot bug #6314

--- src/main/java/ru/hutoroff/fasten/testtask/config/Application.java
+++ src/main/java/ru/hutoroff/fasten/testtask/config/Application.java
@@ -1,12 +1,15 @@
 package ru.hutoroff.fasten.testtask.config;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 /**
  * Created by hutoroff on 06.02.17.
  */
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = HibernateJpaAutoConfiguration.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
